813ec99fb3caf65c6c1a84691e05ada043dfe16c
userrrrnn77 <165572436+userrrrnn77@users.noreply.github.com>
2024-05-05 15:06:42 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const nama = prompt("Hi, isi nama kamu")
+while (true) {
+    let terima = confirm(`${nama}, maafin aku yaa, udah dong jgn ribut teruss`)
+    if (terima) {
+        break
+    }
+}
+
+alert('hehe makasi sayanggg❤❤❤')
